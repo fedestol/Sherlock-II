37728436a7b612a5ac006cc89938e455f84e5ead
fedestol <101721014+fedestol@users.noreply.github.com>
2023-07-11 08:56:22 -0300

    TP PIA

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 #Ejercicio 1
 def digitos(numero_de_tarjeta: str) -> int:
-    return
+    return len(numero_de_tarjeta)
 
 #Ejercicio 2
 def obtener_prefijo(numero_de_tarjeta: str,tamaño_prefijo: int) -> int:
-    return
+    return int(numero_de_tarjeta[:tamaño_prefijo])
 
 #Ejercicio 3
 def tipo_tarjeta(numero_de_tarjeta: str) -> str:
-    condicion_master = False #Pueden borrar el False y completar
-    condicion_visa   = False #Pueden borrar el False y completar
-    condicion_amex   = False #Pueden borrar el False y completar
+    condicion_amex   = bool ((obtener_prefijo(numero_de_tarjeta, 2) == 34 or obtener_prefijo(numero_de_tarjeta, 2) == 37) and digitos(numero_de_tarjeta) == 15)
+    condicion_master = bool (obtener_prefijo(numero_de_tarjeta,2) >= 51 and obtener_prefijo(numero_de_tarjeta, 2)<= 55 and digitos(numero_de_tarjeta) == 16)
+    condicion_visa   = bool(obtener_prefijo(numero_de_tarjeta,1) == 4 and (digitos(numero_de_tarjeta) == 13 or digitos(numero_de_tarjeta) == 16))
     if condicion_master:
         return 'Mastercard'
     elif condicion_visa:
         return 'Visa'
     elif condicion_amex:
         return 'American Express'
+    else:
+        return 'Invalid'
 
 #Ejercicio 4
 def digitos_impares(numero_de_tarjeta : str) -> list[int]:
